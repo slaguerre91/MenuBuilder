d1794402d07bef124983f63e589e8f62963d34f8
Samyr Laguerre <samyrmlaguerre@gmail.com>
2023-04-11 18:57:14 -0400

    placeholder text fix

--- public/index.php
+++ public/index.php
@@ -20,7 +20,7 @@
         <div class="row d-flex justify-content-center mt-4">
             <div class="col-lg-6">
                 <div class="input-group mb-3">
-                    <input type="text" id="search" class="form-control" placeholder="Search for a food item (i.e. pepperoni pizza, caesar salad...)" aria-label="food" aria-describedby="food">
+                    <input type="text" id="search" class="form-control" placeholder="Search for a food item (i.e. pizza, sushi...)" aria-label="food" aria-describedby="food">
                     <div class="input-group-prepend" id="submit">
                         <span class="input-group-text h-100"><i class="fa-solid fa-magnifying-glass"></i></span>
                     </div>
